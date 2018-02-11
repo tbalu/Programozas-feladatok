3cc4b77d2d8c8d50372cbf7a0abf8845e2a0d60e
tbalu <36363537+tbalu@users.noreply.github.com>
2018-02-11 11:34:29 +0100

    Add files via upload

---
+++ pattogolabda.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <curses.h>
+#include <unistd.h>
+
+int main (void)
+{
+	int xj = 0, xk = 0, yj=0, yk=0;
+	int mx = 80*2, my= 24*2;
+
+	WINDOW *ablak;
+	ablak=initscr();
+	noecho ();
+	cbreak ();
+	nodelay (ablak,true);
+for (;;)
+	{
+		xj = (xj-1)%mx;
+		xk = (xk+1)%mx;
+
+		yj=(yj-1)%my;
+		yk=(yk+1)%my;
+		
+		clear ();
+
+		mvprintw (0,0,
+			"--------------------------------------------------------------------------------");
+		mvprintw (24,0,
+			"--------------------------------------------------------------------------------");
+		mvprintw (abs ((yj+(my-yk))/2),
+			  abs ((xj+(mx-xk))/2),"X");
+
+		refresh ();
+		usleep (150000);
+
+	}
+	return 0;
+
+
+}
